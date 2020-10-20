e2ba897b3032a55160695b6802f78b1fb5b32d5e
margothare <margotantigone@gmail.com>
2020-10-19 20:31:19 -0400

    Started making words in the language

--- script.js
+++ script.js
@@ -9,7 +9,41 @@ document.getElementById("run").addEventListener("click", (e) => {
 var lang = [
     {symbol:"love", action: () => {
         console.log("Love ran")
+    }},
+    {symbol:"slowerPerson", action: () => {
+        console.log("Person slows down")
+    }},
+    {symbol:"fasterPerson", action: () => {
+        console.log("Person speeds up")
+    }},
+    {symbol:"buckeye", action: () => {
+        console.log("Buckeye falls")
+    }},
+    {symbol:"badThing", action: () => {
+        console.log("Bad thing falls")
+    }},
+    {symbol:"night", action: () => {
+        console.log("It becomes nighttime")
+    }}, 
+    {symbol:"slowerFall", action: () => {
+        console.log("Things fall down slower")
+    }},
+    {symbol:"fasterFall", action: () => {
+        console.log("Things fall down faster")
+    }},
+    {symbol:"sunglassesOn", action: () => {
+        console.log("Person puts on sunglasses")
+    }},
+    {symbol:"sunglassesOff", action: () => {
+        console.log("Person takes off sunglasses")
+    }},
+    {symbol:"biggerBasket", action: () => {
+        console.log("Basket gets longer")
+    }},
+    {symbol:"smallerBasket", action: () => {
+        console.log("Basket gets shorter")
     }}
+
 ]
 
 function run()  {
