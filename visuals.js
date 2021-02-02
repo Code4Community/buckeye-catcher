98234df052257fc84febecf00daf6cb6c53fccb0
margothare <margotantigone@gmail.com>
2021-02-01 19:57:19 -0500

    first draft of columns

--- visuals.js
+++ visuals.js
@@ -1,2 +1,18 @@
 var c = document.getElementById("game");
 var ctx = c.getContext("2d");
+
+ctx.moveTo(0, 0);
+ctx.lineTo(0, 150);
+ctx.stroke();
+
+ctx.moveTo(85, 150);
+ctx.lineTo(85, 0);
+ctx.stroke();
+
+ctx.moveTo(170, 0);
+ctx.lineTo(170, 150);
+ctx.stroke();
+
+ctx.moveTo(255, 150);
+ctx.lineTo(255, 0);
+ctx.stroke();
