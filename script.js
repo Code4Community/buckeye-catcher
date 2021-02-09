61aa7d918ff023a4630daf3ecb1ea8909046a7e8
ryomakawakami <ryomakwkm@gmail.com>
2021-02-08 20:07:11 -0500

    Added sample game to make sure idea works

--- script.js
+++ script.js
@@ -7,11 +7,36 @@ var editor = CodeMirror.fromTextArea(textArea, {
 });
 editor.setSize("100%", "100%");
 
+// Test game
+class Game {
+    constructor() {
+        this.x = 0;
+    }
+
+    moveLeft() {
+        this.x -= 1;
+        console.log('At position ' + this.x);
+    }
+
+    moveRight() {
+        this.x += 1;
+        console.log('At position ' + this.x);
+    }
+}
+
+game = new Game();  
+
 // Code parsing
 var input;
 
 document.getElementById("run").addEventListener("click", (e) => {
     input = editor.getValue().split(/\s+/);
+
+    if (input.length === 0) {
+        console.log('Error: Code cannot be empty');
+        return;
+    }
+
     if(tokenize(input)) {
         parse(input)
     }
@@ -26,9 +51,11 @@ var lang = [
     }},
     {symbol:"moveLeft",  action: () => {
         console.log("Moving left")
+        game.moveLeft();
     }},
     {symbol:"moveRight",  action: () => {
         console.log("Moving right")
+        game.moveRight();
     }},
     {symbol:"moveTo1",  action: () => {
         console.log("Person speeds up")
