a329a2a96fcfb7996d682f66f883aea1e29ae0ab
margothare <margotantigone@gmail.com>
2021-11-15 20:03:59 -0500

    Updated game_class with creating falling items

--- script.js
+++ script.js
@@ -40,31 +40,6 @@ function create () {
     player.velocityX = 3;
 
     this.physics.add.collider(player, ground);
-
-    // create a falling items group and array with "keys" for either buckeye or michigan logo
-    //fallingItemsBad = this.physics.add.group();
-    //fallingItemsGood = this.physics.add.group();
-
-    this.physics.add.overlap(player, fallingItemsBad, collectMichigans, null, this);
-    this.physics.add.overlap(player, fallingItemsGood, collectBuckeyes, null, this);
-
-    const fallingItemsVals = ['buckeye', 'michigan'];
-    const colLocations = [100, 250, 400, 550, 700, 850, 1000]
-    // go through 50 times and get a value for x and y coordinates (start some at far off x and y values to create timing)
-    // creates 50 falling objects and makes them fall from different heights (to simulate different times)
-    for (let i = 0; i < 50; i++) {
-        let randIndex = Math.round(Math.random());
-        let y = Phaser.Math.Between(0, -11000);
-        let x = colLocations[Phaser.Math.Between(0, 6)];
-        
-        // make falling items and set the scale based on whether its a buckeye or a michigan 
-        if (randIndex == 1) {
-            fallingItemsBad.create(x,y,fallingItemsVals[randIndex]).setScale(0.10).setMaxVelocity(150);
-        } else {
-            fallingItemsGood.create(x,y,fallingItemsVals[randIndex]).setScale(0.26).setMaxVelocity(150);
-        }
-
-    }
             
 }
 
