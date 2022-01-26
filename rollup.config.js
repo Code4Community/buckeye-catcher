a9112899e922b3d48bd0f77a1cb862e1e3091cd0
maxdg99 <maxdg99@gmail.com>
2022-01-26 18:02:30 -0500

    Fixed syntax hightlighting

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,5 @@
 import commonjs from "@rollup/plugin-commonjs"
 import { nodeResolve } from "@rollup/plugin-node-resolve";
-//import postcss from 'rollup-plugin-postcss';
 import css from "rollup-plugin-import-css";
 
 export default {
--- src/page_setup.js
+++ src/page_setup.js
@@ -1,6 +1,5 @@
 import CodeMirror from "codemirror";
 import "codemirror/addon/mode/simple.js";
-import "codemirror/addon/mode/simple.js";
 import "codemirror/lib/codemirror.css";
 
 // Constants
@@ -9,26 +8,24 @@ var EDITOR_ID = "editor"
 // DOM Elements
 var textArea = document.getElementById(EDITOR_ID);
 
-// CodeMirror Setup
-// CodeMirror.defineSimpleMode("mode", {
-//     start: [
-//         { regex: /(?:if|times|forever)\b/, token: "control", indent: true},
-//         { regex: /(?:elif|else)\b/, token: "control", dedent: true, indent: true},
-//         { regex: /(?:end)\b/, token: "control", dedent: true},
-//         { regex: /(?:moveleft|moveright|skip)\b/, token: "statement"},
-//         { regex: /(?:rustle|boom|wind|true|false)\b/, token: "condition"},
-//         { regex: /(?:[1-9][0-9]*)\b/, token: "digits"},
-//     ],
-//     meta: {
-//         electricInput: /elif|else|end/
-//     }
-// });
+//CodeMirror Setup
+CodeMirror.defineSimpleMode("mode", {
+    start: [
+        { regex: /(?:if|times|forever)\b/, token: "control", indent: true},
+        { regex: /(?:elif|else)\b/, token: "control", dedent: true, indent: true},
+        { regex: /(?:end)\b/, token: "control", dedent: true},
+        { regex: /(?:moveleft|moveright|skip)\b/, token: "statement"},
+        { regex: /(?:rustle|boom|wind|true|false)\b/, token: "condition"},
+        { regex: /(?:[1-9][0-9]*)\b/, token: "digits"},
+    ],
+    meta: {
+        electricInput: /elif|else|end/
+    }
+});
 
 var editor = CodeMirror.fromTextArea(textArea, {
     lineNumbers: true,
-    mode: "javascript"
+    mode: "mode"
 });
-
-console.log(editor)
 
 editor.setSize("100%", "100%");
