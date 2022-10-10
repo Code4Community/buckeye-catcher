6983cdc139c7b2222f7aba240e2526b13552f84b
Mackenzie Scott <mscott3327@gmail.com>
2022-10-09 22:40:04 -0400

    Another last ditch effort

--- src/phaser.js
+++ src/phaser.js
@@ -175,11 +175,11 @@ const gameState = {         // Score
 
 var startState = {
     preload: function() {
-        this.load.image('trees', './static/TreesResized.jpg');
-        this.load.image('basket', './static/basket.png');
-        this.load.image('ground', './static/ground.png');
-        this.load.image('buckeye', './static/buckeye-logo.png')
-        this.load.image('michigan', './ichigan_logo.png')
+        this.load.image("trees", "static/TreesResized.jpg");
+        this.load.image("basket", "static/basket.png");
+        this.load.image("ground", "static/ground.png");
+        this.load.image("buckeye", "static/buckeye-logo.png");
+        this.load.image("michigan", "ichigan_logo.png");
     },
 
     create: function() {
